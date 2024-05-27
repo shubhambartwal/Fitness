509e94ce2debfbb565c8fc30d9df3f6f38351ecc
shubhambartwal <shubhambartwal58@gmail.com>
2024-05-27 10:29:54 +0530

    form to add user added

---
+++ frontend/src/components/WorkoutForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const WorkoutForm = () => {
+  const [title, setTitle] = useState("");
+  const [reps, setReps] = useState("");
+  const [load, setLoad] = useState("");
+  const [error, setError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const workout = { title, load, reps };
+    const response = await fetch("http://localhost:4000/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    console.log(response.ok)
+    if (!response.ok) {
+      setError(json);
+    }
+    if (response.ok) {
+      setError(null);
+      setTitle("");
+      setReps("");
+      setLoad("");
+      console.log("New workout Added", json);
+    }
+  };
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a new workout</h3>
+      <label>Exercise title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <label>Load(kg):</label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+      <label>Reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
   useEffect(() => {
     const fetchWorkout = async () => {
-        const response = await fetch('http://localhost:4000/api/workouts');
-    if (response.ok) {
+      const response = await fetch("http://localhost:4000/api/workouts");
+      if (response.ok) {
         const json = await response.json();
-        setWorkouts(json)
-    }
-    
+        setWorkouts(json);
+      }
     };
-    fetchWorkout()
+    fetchWorkout();
   }, []);
   return (
     <div className="workouts">
-      {workouts &&
-        workouts.map((workout) =>(<WorkoutDetails key={workout._id} workout={workout}/>))}
+       {workouts &&
+         workouts.map((workout) => (
+           <WorkoutDetails key={workout._id} workout={workout} />
+     ))}
+      <WorkoutForm />
     </div>
   );
 };
